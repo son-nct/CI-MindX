75516c72e44a563f1a522b045f536c3919569d18
SE140371\WIN 10 <sonstarnguyen@gmail.com>
2021-04-20 23:14:43 +0700

    Homework 1

--- scrip.js
+++ scrip.js
@@ -1,9 +1,15 @@
 const navItems = [
   {
     name: "Item 1",
+    children: ["Item 1.1", "Item 1.2", "Item 1.3"],
   },
   {
     name: "Item 2",
+    children: [
+      "Item 2.1",
+      "Item 2.2",
+      "Item 2.3",
+    ],
   },
   {
     name: "Item 3",
@@ -17,9 +23,26 @@ const navItems = [
 ];
 
 
-window.onload = () => {
-    navItems.forEach(item =>{
-       document.getElementsByClassName('menu')[0].innerHTML += '<span>'+item.name+'</span>';
-    })
-}
+var nav = document.getElementsByClassName('nav');
 
+navItems.forEach((item) => {
+    var span = document.createElement('span');
+    span.appendChild(document.createTextNode(item.name));
+    nav[0].appendChild(span);
+    
+    if(item.children != null) {
+      if(span.textContent === item.name) {
+        var dropdown = document.createElement('div');
+        var ul = document.createElement('ul');
+        dropdown.className = 'dropdown';
+        dropdown.appendChild(ul);
+        for(let value of item.children) {
+          var li = document.createElement('li');
+          li.appendChild(document.createTextNode(value));
+          ul.appendChild(li);          
+        }
+        span.appendChild(dropdown);
+      }
+    }
+   
+});
